c283dc1a6081cf569d84b204c4b65ac61639316c
Bakhtiyar Issakhmetov <random997@gmail.com>
2021-11-08 21:55:34 +0300

    Add test request to spotify search endpoint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <script>
-export default {}
+import spotify from '../plugins/spotify'
+
+export default {
+  mounted() {
+    spotify.get('search', {
+      query: {
+        q: 'Nirvana',
+        type: 'artist'
+      }
+    })
+    .then(response => {
+      console.log(response.data)
+    })
+  }
+}
 </script>
 
 <template>
